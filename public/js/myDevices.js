7e5f71d8b185f18f361b9e1dc0d6685ac96fae75
jason5798 <jason_huang@gemteks.com>
2017-06-20 09:24:31 +0800

    [0.0.1]Init first version

---
+++ public/js/myDevices.js
@@ -0,0 +1,342 @@
+console.log("Message admin device information");
+var connected = false;
+var host = window.location.hostname;
+var port = window.location.port;
+var opt2={
+    //"order": [[ 2, "desc" ]],
+    "iDisplayLength": 100,
+    dom: 'Blrtip',
+    buttons: [
+        'copyHtml5',
+        //'excelHtml5',
+        'csvHtml5',
+        //'pdfHtml5'
+    ]
+};
+var table = $('#table1').dataTable(opt2);
+
+var plot1,plot2,plot3,plot4,plot5,plot6;
+var options1 ={
+                title: "PH",
+                axes:
+                {
+                    xaxis: {
+                        numberTicks: 24,
+                        renderer:$.jqplot.DateAxisRenderer,
+                        tickOptions:{formatString:'%H:%M'}
+                    },
+                    yaxis: {
+                        numberTicks: 10
+                    }
+                }
+            };
+var options2 ={
+                title: "DO",
+                axes:
+                {
+                    xaxis: {
+                        numberTicks: 24,
+                        renderer:$.jqplot.DateAxisRenderer,
+                        tickOptions:{formatString:'%H:%M'}
+                    },
+                    yaxis: {
+                        numberTicks: 10
+                    }
+                }
+            };
+var options2 ={
+                title: "DO",
+                axes:
+                {
+                    xaxis: {
+                        numberTicks: 24,
+                        renderer:$.jqplot.DateAxisRenderer,
+                        tickOptions:{formatString:'%H:%M'}
+                    },
+                    yaxis: {
+                        numberTicks: 10
+                    }
+                }
+            };
+var options3 ={
+                title: "COND",
+                axes:
+                {
+                    xaxis: {
+                        numberTicks: 24,
+                        renderer:$.jqplot.DateAxisRenderer,
+                        tickOptions:{formatString:'%H:%M'}
+                    },
+                    yaxis: {
+                        numberTicks: 10
+                    }
+                }
+            };
+
+var options4 ={
+                title: "Temperature",
+                axes:
+                {
+                    xaxis: {
+                        numberTicks: 24,
+                        renderer:$.jqplot.DateAxisRenderer,
+                        tickOptions:{formatString:'%H:%M'}
+                    },
+                    yaxis: {
+                        numberTicks: 10
+                    }
+                }
+            };
+var options5 ={
+                title: "NTU",
+                axes:
+                {
+                    xaxis: {
+                        numberTicks: 24,
+                        renderer:$.jqplot.DateAxisRenderer,
+                        tickOptions:{formatString:'%H:%M'}
+                    },
+                    yaxis: {
+                        numberTicks: 10
+                    }
+                }
+            };
+
+var options6 ={
+                title: "Voltage",
+                axes:
+                {
+                    xaxis: {
+                        numberTicks: 24,
+                        renderer:$.jqplot.DateAxisRenderer,
+                        tickOptions:{formatString:'%H:%M'}
+                    },
+                    yaxis: {
+                        numberTicks: 10
+                    }
+                }
+            };
+
+if(location.protocol=="https:"){
+  var wsUri="wss://"+host+":"+port+"/ws/devices";
+} else {
+  var wsUri="ws://"+host+":"+port+"/ws/devices";
+}
+console.log(wsUri);
+var ws=null;
+
+function wsConn() {
+  ws = new WebSocket(wsUri);
+  ws.onmessage = function(m) {
+    //console.log('< from-node-red:',m.data);
+    if (typeof(m.data) === "string" && m. data !== null){
+      var msg =JSON.parse(m.data);
+      console.log("from-node-red : id:"+msg.id);
+      if(msg.id === 'init_table'){
+          //Remove init button active
+          //console.log("v : "+msg.v);
+
+          //Reload table data
+          //console.log("v type:"+typeof(msg.v));
+
+          table.fnClearTable();
+          var data = JSON.parse(msg.v);
+          //console.log("addData type : "+ typeof(data)+" : "+data);
+          if(data){
+              table.fnAddData(data);
+          }
+          waitingDialog.hide();
+          showChart(data);
+      }
+    }
+  }
+  ws.onopen = function() {
+    var mac = document.getElementById("mac").value;
+    var type = document.getElementById("type").value;
+    var date = document.getElementById("date").value;
+    var option= document.getElementById("option").value;
+    var host = window.location.hostname;
+    var port = window.location.port;
+    var json = {mac:mac,type:type,date:date,option:option,host:host,port:port};
+    //alert('date :'+ date);
+    connected = true;
+    var obj = {"id":"init","v":json};
+    var getRequest = JSON.stringify(obj);
+    console.log("getRequest type : "+ typeof(getRequest)+" : "+getRequest);
+    console.log("ws.onopen : "+ getRequest);
+    ws.send(getRequest);      // Request ui status from NR
+    console.log(getRequest);
+
+  }
+  ws.onclose   = function()  {
+    console.log('Node-RED connection closed: '+new Date().toUTCString());
+    connected = false;
+    ws = null;
+  }
+  ws.onerror  = function(){
+    console.log("connection error");
+  }
+}
+wsConn();           // connect to Node-RED server
+
+function setButton(_id,_v){ // update slider
+  myselect = $("#"+_id);
+   myselect.val(_v);
+   myselect.slider('refresh');
+}
+
+function toSecondTable(mac){
+    //alert("mac : "+mac);
+    //document.location.href="/device?mac="+mac;
+}
+
+function showDialog(){
+    //waitingDialog.show('Custom message', {dialogSize: 'sm', progressType: 'warning'});
+    waitingDialog.show();
+    setTimeout(function () {
+      waitingDialog.hide();
+      }, 5000);
+}
+
+function back(){
+    //alert('back');
+    location.href=document.referrer;
+}
+
+function showChart(data){
+    var tempList = [],doList=[];
+    var condList = [],ntuList=[];
+    var phList = [],volList=[];
+    //alert('data.length : '+data.length);
+    for(i=0;i<data.length;i++){
+        //alert('i : '+i);
+        if(i > (data.length-1) ){
+            break;
+        }else{
+
+            phList.push([new Date(data[i][1]).getTime(),data[i][3] ]);
+            doList.push([new Date(data[i][1]).getTime(),data[i][4] ]);
+            condList.push([new Date(data[i][1]).getTime(),data[i][5] ]);
+            tempList.push([new Date(data[i][1]).getTime(),data[i][6] ]);
+            ntuList.push([new Date(data[i][1]).getTime(),data[i][7] ]);
+            volList.push([new Date(data[i][1]).getTime(),data[i][8] ]);
+        }
+
+        /*if(i<2){
+          alert('data[i] : '+JSON.stringify(data[i]) );
+        alert('data[i][8] : '+data[i][3]);
+        alert('data[i][8] : '+data[i][4]);
+        alert('data[i][8] : '+data[i][5]);
+        alert('data[i][8] : '+data[i][6]);
+        alert('data[i][8] : '+data[i][7]);
+        alert('data[i][8] : '+data[i][8]);
+        }*/
+
+    }
+
+
+    tatalTime = Math.ceil((tempList[tempList.length-1][0]-tempList[0][0])/(1000*60*60))+1;
+    //alert( tatalTime );
+    if(tatalTime<12){
+        x_number = tatalTime;
+        formatStr = '%H:%M';
+    }else if(tatalTime<26){
+        x_number = tatalTime/2;
+        formatStr = '%H:%M';
+    }else if(tatalTime<24*7){
+        x_number = Math.ceil(tatalTime/24);
+        formatStr = '%m/%d';
+    }else if(tatalTime<24*31){
+        x_number = Math.ceil(tatalTime/(2*24));
+        formatStr = '%m/%d';
+    }else if(tatalTime<24*31*3){
+        x_number = Math.ceil(tatalTime/(7*24));
+        formatStr = '%m/%d';
+    }
+
+
+    if(plot1){
+        plot1.destroy();
+        plot2.destroy();
+        plot3.destroy();
+        plot4.destroy();
+        plot5.destroy();
+        plot6.destroy();
+
+    }
+
+    options1.axes.xaxis.numberTicks = x_number;
+    options2.axes.xaxis.numberTicks = x_number;
+    options3.axes.xaxis.numberTicks = x_number;
+    options4.axes.xaxis.numberTicks = x_number;
+    options5.axes.xaxis.numberTicks = x_number;
+    options6.axes.xaxis.numberTicks = x_number;
+    options1.axes.xaxis.tickOptions.formatString = formatStr;
+    options2.axes.xaxis.tickOptions.formatString = formatStr;
+    options3.axes.xaxis.tickOptions.formatString = formatStr;
+    options4.axes.xaxis.tickOptions.formatString = formatStr;
+    options5.axes.xaxis.tickOptions.formatString = formatStr;
+    options6.axes.xaxis.tickOptions.formatString = formatStr;
+    //Jason add for chart to image on 2017.06.12
+
+    plot1 = $.jqplot ('chartPH', [phList],options1);
+    plot2 = $.jqplot ('chartDO', [doList],options2);
+    plot3 = $.jqplot ('chartCOND', [condList],options3);
+    plot4 = $.jqplot ('chartTmp', [tempList],options4);
+    plot5 = $.jqplot ('chartNTU', [ntuList],options5);
+    plot6 = $.jqplot ('chartVol', [volList],options6);
+    var imgPHData = $('#chartPH').jqplotToImageStr({});
+    var imgPHElem = $('<img/>').attr('src',imgPHData);
+    $('#imgChart1').append(imgPHElem);
+    $("#chartPH").hide();
+    var imgDOData = $('#chartDO').jqplotToImageStr({});
+    var imgDOElem = $('<img/>').attr('src',imgDOData);
+    $('#imgChart2').append(imgDOElem);
+    $("#chartDO").hide();
+    var imgCONDData = $('#chartCOND').jqplotToImageStr({});
+    var imgCONDElem = $('<img/>').attr('src',imgCONDData);
+    $('#imgChart3').append(imgCONDElem);
+    $("#chartCOND").hide();
+    var imgTmpData = $('#chartTmp').jqplotToImageStr({});
+    var imgTmpElem = $('<img/>').attr('src',imgTmpData);
+    $('#imgChart4').append(imgTmpElem);
+    $("#chartTmp").hide();
+    var imgNTUData = $('#chartNTU').jqplotToImageStr({});
+    var imgNTUElem = $('<img/>').attr('src',imgNTUData);
+    $('#imgChart5').append(imgNTUElem);
+    $("#chartNTU").hide();
+    var imgVolData = $('#chartVol').jqplotToImageStr({});
+    var imgVolElem = $('<img/>').attr('src',imgVolData);
+    $('#imgChart6').append(imgVolElem);
+    $("#chartVol").hide();
+}
+
+function changeChartDisplay(){
+  var obj = document.getElementById("chartBtn");
+  alert(obj.value);
+  if(obj.value === 'Show Chart'){
+    obj.value = 'Hide Chart';
+   $("#chartBlock").hide();
+  }else{
+    obj.value = 'Show Chart';
+    $("#chartBlock").show();
+  }
+}
+
+$(document).ready(function(){
+    showDialog();
+
+
+  $('#chartBlock').tabs({
+    onSelect:function(title){
+        if(title==='DO'){
+              alert(title);
+             $('#my_tabs').tabs('load', 0);
+        }
+      },
+      onLoad:function(panel){
+        alert(panel.panel('options').title+' is loaded')
+      }
+    })
+
+});
---
+++ routes/index.js
@@ -0,0 +1,193 @@
+﻿var express = require('express');
+var router = express.Router();
+var DeviceDbTools = require('../models/deviceDbTools.js');
+var ListDbTools = require('../models/listDbTools.js');
+var UnitDbTools = require('../models/unitDbTools.js');
+var settings = require('../settings');
+var JsonFileTools =  require('../models/jsonFileTools.js');
+var path = './public/data/finalList.json';
+var path2 = './public/data/test.json';
+var unitPath = './public/data/unit.json';
+var selectPath = './public/data/select.json';
+var hour = 60*60*1000;
+var type = 'gps';
+
+function findUnitsAndShowSetting(req,res,isUpdate){
+	UnitDbTools.findAllUnits(function(err,units){
+		var successMessae,errorMessae;
+		var macTypeMap = {};
+
+		if(err){
+			errorMessae = err;
+		}else{
+			if(+units.length>0){
+				successMessae = '查詢到'+units.length+'筆資料';
+			}
+		}
+		req.session.units = units;
+
+		console.log( "successMessae:"+successMessae );
+		res.render('setting', { title: 'Setting',
+			units:req.session.units,
+			user:req.session.user,
+			success: successMessae,
+			error: errorMessae
+		});
+	});
+}
+
+module.exports = function(app) {
+  app.get('/', function (req, res) {
+  	    var now = new Date().getTime();
+		var selectObj = JsonFileTools.getJsonFromFile(selectPath);
+	
+		ListDbTools.findByName('finalist',function(err,lists){
+			if(err){
+				res.render('index', { title: 'Index',
+					success: '',
+					error: err.toString(),
+					finalList:null,
+					type:type,
+					isNeedTypeSwitch:settings.isNeedTypeSwitch,
+					co:settings.co
+				});
+			}else{
+                var finalList = lists[0]['list'];
+				var unitObj = JsonFileTools.getJsonFromFile(unitPath);
+
+				//console.log('finalList :'+JSON.stringify(finalList));
+				if(finalList){
+					var keys = Object.keys(finalList);
+					console.log('Index finalList :'+keys.length);
+					for(var i=0;i<keys.length ;i++){
+						//console.log( i + ') mac : ' + keys[i] +'=>' + JSON.stringify(finalList[keys[i]]));
+						//console.log(i+' result : '+ ((now - finalList[keys[i]].timestamp)/hour));
+						finalList[keys[i]].overtime = true;
+						if( ((now - finalList[keys[i]].timestamp)/hour) < 2 )  {
+							finalList[keys[i]].overtime = false;
+						}
+						finalList[keys[i]].name = '';
+						//console.log(i+' keys[i] : '+ keys[i]);
+						//console.log(i+' unitObj[keys[i]] : '+ unitObj[keys[i]]);
+						if( unitObj[keys[i]] )  {
+							finalList[keys[i]].name = unitObj[keys[i]];
+						}
+					}
+				}else{
+					finalList = null;
+				}
+
+				res.render('index', { title: 'Index',
+					success: null,
+					error: null,
+					finalList:finalList,
+					type:type,
+					isNeedTypeSwitch:settings.isNeedTypeSwitch,
+					co:settings.co,
+					select:selectObj
+				});
+			}
+		});
+  });
+
+  app.get('/devices', function (req, res) {
+	var mac = req.query.mac;
+	var type = req.query.type;
+	var date = req.query.date;
+	var option = req.query.option;
+	req.session.type = type;
+	DeviceDbTools.findDevicesByDate(date,mac,Number(0),'desc',function(err,devices){
+		if(err){
+			console.log('find name:'+find_mac);
+			return;
+		}
+		var length = 15;
+		if(devices.length<length){
+			length = devices.length;
+		}
+
+		/*devices.forEach(function(device) {
+			console.log('mac:'+device.date + ', data :' +device.data);
+		});*/
+
+		res.render('devices', { title: 'Device',
+			devices: devices,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString(),
+			type:req.session.type,
+			mac:mac,
+			date:date,
+			option:option,
+			length:length,
+			isNeedTypeSwitch:settings.isNeedTypeSwitch,
+			co:settings.co
+		});
+	});
+  });
+
+  app.get('/setting', function (req, res) {
+		console.log('render to setting.ejs');
+		findUnitsAndShowSetting(req,res,true);
+  });
+
+  app.post('/setting', function (req, res) {
+		var	post_mac = req.body.mac;
+		var post_name = req.body.name;
+		var post_type = req.body.type_option;
+		var post_mode = req.body.mode;
+		var typeString = req.body.typeString;
+		console.log('mode : '+post_mode);
+		if(post_mode == 'new'){
+			if(	post_mac && post_name && post_mac.length==8 && post_name.length>=1){
+				console.log('post_mac:'+post_mac);
+				console.log('post_name:'+post_name);
+				UnitDbTools.saveUnit(post_mac,post_name,post_type,typeString,function(err,result){
+					if(err){
+						req.flash('error', err);
+						return res.redirect('/setting');
+					}
+					findUnitsAndShowSetting(req,res,true);
+				});
+				var unitObj = JsonFileTools.getJsonFromFile(unitPath);
+				unitObj[post_mac] = post_name;
+				JsonFileTools.saveJsonToFile(unitPath,unitObj);
+				return res.redirect('/setting');
+			}
+		}else if(post_mode == 'del'){//Delete mode
+			post_mac = req.body.postMac;
+			UnitDbTools.removeUnitByMac(post_mac,function(err,result){
+				if(err){
+					req.flash('error', err);
+					console.log('removeUnitByMac :'+post_mac + err);
+					return res.redirect('/setting');
+				}else{
+					req.flash('error', err);
+					console.log('removeUnitByMac :'+post_mac + 'success');
+				}
+				findUnitsAndShowSetting(req,res,false);
+			});
+			var unitObj = JsonFileTools.getJsonFromFile(unitPath);
+			if(unitObj[post_mac]){
+				delete unitObj[post_mac];
+			}
+			
+			JsonFileTools.saveJsonToFile(unitPath,unitObj);
+
+		}else{//Edit mode
+			post_mac = req.body.postMac;
+			UnitDbTools.updateUnit(post_type,post_mac,post_name,null,typeString,function(err,result){
+				if(err){
+					req.flash('error', err);
+					console.log('edit  :'+post_mac + err);
+					return res.redirect('/setting');
+				}else{
+					console.log('edit :'+post_mac + 'success');
+				}
+				findUnitsAndShowSetting(req,res,false);
+			});
+     		var unitObj = JsonFileTools.getJsonFromFile(unitPath);
+			unitObj[post_mac] = post_name;
+			JsonFileTools.saveJsonToFile(unitPath,unitObj);
+		}
+  	});
+};
